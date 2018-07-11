5aa4b849f372667a2f752ea31e2295c8ca0a91c1
LiouYuCi <original541029@gmail.com>
2018-07-11 08:36:05 +0800

    add style and select option js

--- all.js
+++ all.js
@@ -1,6 +1,6 @@
 function GetOpendata() {
     return new Promise(function(resolve, reject) {
-        fetch('https://tpeoc.blob.core.windows.net/blobfs/GetAccommodatePlaceData.json')
+        fetch('https://tpeoc.blob.core.windows.net/blobfs/GetDisasterSummary.json')
             .then(function(response) {
                 return response.json();
             })
@@ -10,14 +10,28 @@ function GetOpendata() {
     });
 }
 var OpenData = GetOpendata();
+// var opendata;
 OpenData.then(function(result) {
-    console.log(result);
-    var opendata = result;
-    var opendatalen = opendata.length;
-    var ListSelect = document.querySelector('.list-select');
-    var selectstr = '<option value="-- 請選擇行政區 --">-- 請選擇行政區 --</option>';
-    for (i = 0; i < opendatalen; i++) {
-        selectstr += '<option value="' + opendata[i].District + '">' + opendata[i].District + '</option>'
-    }
-    ListSelect.innerHTML = selectstr;
+    opendata = result;
+    SelectArea();
 })
+var ListSelect = document.querySelector('#areaId');
+
+function SelectArea(e) {
+    console.log(e)
+    var opendatalen = opendata.length;
+    let filterOpendata = [];
+    var selectstr = '';
+    for (i = 0; i < opendatalen; i++) {
+        if (filterOpendata.indexOf(opendata[i].CaseLocationDistrict) < 0) {
+            filterOpendata.push(opendata[i].CaseLocationDistrict)
+            selectstr += '<option value="' + opendata[i].CaseLocationDistrict + '">' + opendata[i].CaseLocationDistrict + '</option>'
+        }
+    }
+    ListSelect.innerHTML = '<option value disabled selected>-- 請選擇行政區 --</option>' + selectstr;
+}
+ListSelect.addEventListener('change', SelectArea, false);
+
+// function ClickUpdataTabledata(e) {
+
+// }
